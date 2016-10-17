85beaab48e571d4ed25ec78dd013848d79a30805
Thiago Bezerra <bezerrathm@gmail.com>
2016-10-17 12:43:30 -0300

    #3

--- test/test1.cpp
+++ test/test1.cpp
@@ -2,20 +2,15 @@
 
 int main(int argc, char** argv)
 {
-
-	int z = argc + 1;
-
 	int a[] = {1,2,3,4,5,6,7};
-	int b[] = {1,2,3,4,5,6,z};
+	int b[] = {1,2,3,4,5,6,7};
 
 	int c = 100;
 
 	for (int i = 3;i < 5; i++)
 	{
-		a[i] = b[z + 1] + c;
+		a[i] = b[i + 1] + c;
 	}
 
-	z += a[0];
-
-	return z;
+	return a[argc];
 }
