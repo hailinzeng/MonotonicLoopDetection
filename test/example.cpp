064d227077c17a72f60074a0e9bab77eddc06910
Thiago Bezerra <bezerrathm@gmail.com>
2016-11-12 15:29:40 -0300

    minor fix

--- test/example.cpp
+++ test/example.cpp
@@ -2,18 +2,16 @@
 // RUN: %opt -instnamer -mem2reg -break-crit-edges %t1.bc -o %t1.bc
 // RUN: %clang -std=c++11 -O0 -w -c -emit-llvm -Xclang -load -Xclang %S/../../build/lib/MonotonicLoopDetection.so %t1.bc -o - | %lli | FileCheck %s
 
-// CHECK: Monotonic loop detected!
-
-
 #include <stdio.h>
 int main()
 {
     int arr[100];
 
-    printf(" ... here MLD pass should be inserting printf with 'Monotonic...' .");
-    for (int i = 0 ; i < 15; i++){
+    //CHECK: Monotonic loop detected
+    for (int i = 0 ; i < 15; i++) { 
        arr[i] = i+1;
     }
 
     return 0;
 }
+
