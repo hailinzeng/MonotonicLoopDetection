a2702654f16e5bcea2e564480293a94ac080efa5
hailinzeng <hailinzeng@gmail.com>
2016-12-29 23:57:45 +0800

    check up bound

---
+++ test/bounddecult.cpp
@@ -0,0 +1,19 @@
+// RUN: %S/../../../build/bin/clang++ -std=c++11 -w -O0 -c -emit-llvm %s -o %t1.bc
+// RUN: %S/../../../build/bin/opt -loop-extract  -instnamer -mem2reg -break-crit-edges %t1.bc -o %t1.bc
+// RUN: %S/../../../build/bin/opt -mld %t1.bc -o - | llvm-dis | FileCheck %s
+
+// CHECK: is.monotonic
+
+// CHECK: call i1 @__check_array_min
+// CHECK: call i1 @__check_array_max
+
+int main() {
+    int* arr = new int[32];
+    arr[31] = 0;
+    for (unsigned i = 31-1; 0 < i; i--) {
+        arr[i] = arr[i+1] + i;
+        // if (i == 0)
+        //     break;
+    }
+    return 0;
+}
---
+++ test/boundinculer.cpp
@@ -0,0 +1,17 @@
+// RUN: %S/../../../build/bin/clang++ -std=c++11 -w -O0 -c -emit-llvm %s -o %t1.bc
+// RUN: %S/../../../build/bin/opt -loop-extract  -instnamer -mem2reg -break-crit-edges %t1.bc -o %t1.bc
+// RUN: %S/../../../build/bin/opt -mld %t1.bc -o - | llvm-dis | FileCheck %s
+
+// CHECK: is.monotonic
+
+// CHECK: call i1 @__check_array_min
+// CHECK: call i1 @__check_array_max
+
+int main() {
+    int* arr = new int[32];
+    arr[0] = 0;
+    for (unsigned i = 1; 31 >= i; i++) {
+        arr[i] = arr[i-1] + i;
+    }
+    return 0;
+}
